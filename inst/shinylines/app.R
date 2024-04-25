1fab6dbd21b1c1a9c008459f43e80b51222f7520
Brandan Rosa <brandanrosa22@gmai.com>
2024-04-25 04:04:30 -0500

    shiny app

---
+++ inst/shinylines/app.R
@@ -0,0 +1,94 @@
+spruce <- read.csv("SPRUCE.csv")
+
+ylm <- lm(Height ~ BHDiameter, data = spruce)
+sm <- summary(ylm)
+
+##################################################################
+library(ggplot2)
+library(shiny)
+library(shinythemes)
+
+# UI
+ui <- fluidPage(
+  theme = shinytheme("slate"),
+
+    # Application Title
+    titlePanel("Spruce Regression Analysis"),
+
+    # Sidebar Panel
+    sidebarLayout(
+      sidebarPanel(
+        varSelectInput(inputId = "xvar",
+                       "Explanatory variable (x):",
+                       spruce[,c("Height", "BHDiameter")],
+                       selected =  "BHDiameter"),
+                         
+        varSelectInput(inputId = "yvar",
+                       "Response variable (y):",
+                       spruce[,c("Height", "BHDiameter")],
+                       selected =  "Height")
+                  ),
+                         
+  mainPanel(
+    
+    fluidRow(
+      column(12, 
+             wellPanel(plotOutput("scatter")))),
+    
+    fluidRow(
+      column(12,
+             wellPanel(plotOutput("ldat")))),
+    
+    fluidRow(
+      column(12,
+             wellPanel(plotOutput("qdat")))),
+    
+    fluidRow(
+      column(12,
+             wellPanel(plotOutput("qres"))))
+    
+     )
+    )
+   )
+
+# Server
+server <- function(input, output) {
+  
+  dfmod <- within(spruce, {
+    resid <- ylm$residuals
+    yhat <- ylm$fitted.values
+    }
+  )
+
+  # Scatter with Smoother
+   output$scatter <- renderPlot({
+     ggplot(data = dfmod, aes(x = !!input$xvar, y = !!input$yvar)) +
+       geom_point(color = "hotpink", cex = 2) +
+       stat_smooth(color = "darkgreen") +
+       labs(title = "Spruce Data Plot", subtitle = "LOESS Smoother") 
+    })
+   
+   output$ldat <- renderPlot({
+     ggplot(data = dfmod, aes(x = !!input$xvar, y = !!input$yvar)) +
+      geom_point(color = "navy", cex = 2) +
+       stat_smooth(method = "lm", formula = y ~ x, color = "maroon") +
+       labs(title = "Plot of the Data", subtitle = "Linear Model")
+   })
+   
+   output$qdat <- renderPlot({
+     ggplot(data = dfmod, aes(x = !!input$xvar, y = !!input$yvar)) +
+       geom_point(color = "red", cex = 2) +
+       stat_smooth(method = "lm", formula = y ~ x + I(x^2), color = "black") +
+       labs(title = "Plot of the Data", subtitle =  "Quadratic Model")
+   })
+   
+   output$qres <- renderPlot({
+     ggplot(data = dfmod, aes(x = yhat, y = resid)) +
+       geom_point(color = "darkgreen", cex = 2) +
+       stat_smooth(color = "gold") +
+       labs(title = "Plot of the Data", subtitle =  "LOESS Smoother")
+   })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
